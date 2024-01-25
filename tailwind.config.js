103e5a2abf3e7dfb94458efcaf3c23e92953c16e
stephtheitsloth <stephan.karim.sk@gmail.com>
2024-01-25 18:30:32 -0500

    primary configurations done and background on

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    spacing: {
+      xxs: "4",
+      xs: "8",
+      sm: '12px',
+      md: '16px',
+      lg: '24px',
+      '2lg': '32px',
+      xl: '48px',
+      '2xl': '64px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
